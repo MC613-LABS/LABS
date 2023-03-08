3cdd262ea46308af37c25f8d7dbc3a3e9aefd211
João Pedro Carolino Morais <96194625+jpcmorais16@users.noreply.github.com>
2023-03-08 16:34:48 -0300

    Add files via upload

--- minimizado.vhd
+++ minimizado.vhd
@@ -12,9 +12,9 @@ ARCHITECTURE LogicFunction of minimizado IS
 signal x1, x2, x3, x4, x5 : STD_LOGIC;
 BEGIN
 	f <= x1 or x2 or x3 or x4 or x5;
-	x1 <= b and c and e;
-	x2 <= not a and not b and not c and not e;
-	x3 <= not b and not c and d and not e;
-	x4 <= not a and c and not d and e;
+	x1 <= c and not d and e;
+	x2 <= b and c and e;
+	x3 <= not a and not b and not c and not e;
+	x4 <= not b and not c and d and not e;
 	x5 <= b and not c and not d and not e;
 END LogicFunction;
